40c9aedaa600e4873feba00778f1433eb9dd7f55
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2022-04-03 14:36:56 +0300

    first commit

---
+++ src/main/java/com/freesoft/myblogspringboot/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.freesoft.myblogspringboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blogs")
+public class MainController {
+
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+
+
+}
---
+++ src/test/java/com/freesoft/myblogspringboot/MyBlogSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freesoft.myblogspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
